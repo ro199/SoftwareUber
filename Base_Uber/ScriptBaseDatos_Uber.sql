406636c23ba86c433fc1ab438843224841e97d29
ChristianB <christian.benitez@epn.edu.ec>
2019-07-16 16:25:32 -0500

    base de datos actualizada(nueva)
    Borrar anterior y crear de nuevo la base de datos.

--- Base_Uber/ScriptBaseDatos_Uber.sql
+++ Base_Uber/ScriptBaseDatos_Uber.sql
@@ -1,17 +1,28 @@
-
+-- drop database uber;
+create database uber;
 use uber;
+
 /*----------------------------------------------------------*/
 /*---------------- Creación tablas--------------------------*/
 /*----------------------------------------------------------*/
 
 create table usuario(
-	cedulaUsuario varchar(10) primary key not null,
+	idTipo int primary key not null auto_increment,
+    usuario varchar(30) not null,
+    contrasena varchar(30),
+	tipoUsuario varchar(20) not null
+);
+
+create table cliente(
+	cedulaCliente varchar(10) primary key not null,
     nombre varchar(30) not null,
     apellido varchar(30) not null,
     correoU varchar(50) not null,
     telefonoU varchar(15) not null,
-    contrasenaU varchar(25) not null
+    idTipo int not null,
+    FOREIGN KEY (idTipo) REFERENCES usuario(idTipo)
 );
+
 
 create table tarjeta(
 	idTarjeta int auto_increment primary key,
@@ -23,12 +34,23 @@ create table tarjeta(
 create table usuario_tarjeta(
 	cedulaUsuario varchar(10)  not null,
     idTarjeta int,
-    
-    FOREIGN KEY (cedulaUsuario) REFERENCES usuario(cedulaUsuario),
+    cedulaCliente varchar(10) not null,
+    FOREIGN KEY (cedulaCliente) REFERENCES cliente(cedulaCliente),
     FOREIGN KEY (idTarjeta) REFERENCES tarjeta(idTarjeta)
 
 );
 
+create table vehiculo(
+	matricula varchar(15) not null primary key,
+    marca varchar(30) not null,
+    color varchar(30) not null,
+    foto varchar(100)
+);
+
+create table asociacion(
+	idAsociacion int primary key not null auto_increment,
+    asociacion varchar(30) not null
+);
 
 create table conductor(
 	cedulaConductor varchar(10) primary key not null,
@@ -39,17 +61,15 @@ create table conductor(
     fechaNacimiento varchar(15) not null,
 	direccion varchar(100) not null,
     nacionalidad varchar(50) not null,
-    contrasenaC varchar(25) not null,
     matricula varchar(15) not null,
-    FOREIGN KEY (matricula) REFERENCES vehiculo(matricula)
+    idAsociacion int not null,
+    idTipo int not null,
+    FOREIGN KEY (matricula) REFERENCES vehiculo(matricula),
+    FOREIGN KEY (idAsociacion) REFERENCES asociacion(idAsociacion),
+    FOREIGN KEY (idTipo) REFERENCES usuario(idTipo)
 );
 
-create table vehiculo(
-	matricula varchar(15) not null primary key,
-    marca varchar(30) not null,
-    color varchar(30) not null,
-    foto varchar(100)
-);
+
 
 
 /*----------------------------------------------------------*/
@@ -57,11 +77,10 @@ create table vehiculo(
 /*----------------------------------------------------------*/
 
 select * from usuario;
+select * from cliente;
 select * from tarjeta;
 select * from usuario_tarjeta;
 select * from conductor;
 select * from vehiculo;
-
-
-
+select * from asociacion;
 
